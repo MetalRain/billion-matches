4d69941636bfe588838117bb7e4ad7be9b06f730
Otto Martikainen <martikainenotto@gmail.com>
2023-03-28 09:43:19 +0300

    Refactor example to functions

--- solutions/example/main.py
+++ solutions/example/main.py
@@ -5,28 +5,30 @@ from typing import Dict, Tuple
 
 from tqdm import tqdm
 
+TimeIdMap = Dict[int, int]
+LabelMap = Dict[int, TimeIdMap]
+Subscriber = Tuple[int, list[int]]
+
 def main() -> None:
     parser = argparse.ArgumentParser(description='Billion matches example')
-    parser.add_argument('labels')
     parser.add_argument('items')
     parser.add_argument('subscribers')
     args = parser.parse_args()
 
-    # group items by label
-    label_item_map: Dict[int, Dict[int, int]] = {}
-
-    # read labels
-    with open(args.labels) as f:
-        r = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
-        # skip header line
-        r.__next__()
-        for row in tqdm(r, desc='Read labels'):
-            label_item_map[int(row[0])] = {}
-
+    label_item_map: LabelMap = read_items(args.items)
     gc.collect()
+
+    subscribers: list[Tuple[int, list[int]]]  = read_subscribers(args.subscribers)
+    gc.collect()
+
+    write_result(subscribers, label_item_map)
+
+def read_items(filepath: str) -> LabelMap:
+    # group items by label
+    label_item_map: LabelMap = {}
         
     # read items
-    with open(args.items) as f:
+    with open(filepath) as f:
         r = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
         # skip header line
         r.__next__()
@@ -34,15 +36,18 @@ def main() -> None:
             id = int(row[0])
             ts = int(row[1])
             for l in split_ids(row[2]):
+                if l not in label_item_map:
+                    label_item_map[l] = {}
                 # here we have assumption: one timestamp has only one item
                 label_item_map[l][ts] = id
 
-    gc.collect()
+    return label_item_map
 
-    subscribers: list[Tuple[int, list[int]]]  = []
+def read_subscribers(filepath: str) -> list[Subscriber]:
+    subscribers: list[Subscriber]  = []
 
     # read subscribers
-    with open(args.subscribers) as f:
+    with open(filepath) as f:
         r = csv.reader(f, delimiter=',', quoting=csv.QUOTE_NONE)
         # skip header line
         r.__next__()
@@ -52,20 +57,20 @@ def main() -> None:
                 split_ids(row[1])
             ))
 
-    gc.collect()
-
     # sort subscribers
     subscribers.sort(key=lambda sub: sub[0])
+    return subscribers
 
-    # write result
+def write_result(subscribers, label_item_map) -> None:
     with open('./output/output.csv', 'w') as f:
         w = csv.writer(f, delimiter=',', quoting=csv.QUOTE_NONE)
         w.writerow(['subscriber_id', 'item_ids'])
 
         for s_id, s_labels in tqdm(subscribers, desc='Process deliveries'):
             # collect matching items
+
             # dict deduplicates items
-            items_to_deliver: Dict[int, int] = {}
+            items_to_deliver: TimeIdMap = {}
             for l in s_labels:
                 items_to_deliver.update(label_item_map[l])
 
@@ -76,7 +81,7 @@ def main() -> None:
             ]
             w.writerow(row)
 
-def split_ids(id_str) -> list[int]:
+def split_ids(id_str: str) -> list[int]:
     return [int(id) for id in id_str.split('|')]
 
 def join_ids(ids: list[int]) -> str:
